97d5369d6fbe319a42400bab19f20aef3b830beb
PatilKartik1 <patilkartik94@gmail.com>
2026-05-11 16:27:12 +0530

    added pages folder in src and added home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,5 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Stats from "./components/Stats";
-
 function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-black via-zinc-900 to-black">
-      <Navbar />
-      <Hero />
-      <Features />
-      <Stats />
-    </div>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
