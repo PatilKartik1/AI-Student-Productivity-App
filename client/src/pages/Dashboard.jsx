36474d2cbfa51751e3d11d8192197c1c9eec28b5
Patilkartik1 <132817019+PatilKartik1@users.noreply.github.com>
2026-05-12 23:46:38 +0530

    added Dashboard page

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+  return (
+    <div className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-5xl font-bold">Dashboard</h1>
+
+      <p className="text-zinc-400 mt-4">Welcome back, Kartik 👋</p>
+    </div>
+  );
+}
+
+export default Dashboard;
