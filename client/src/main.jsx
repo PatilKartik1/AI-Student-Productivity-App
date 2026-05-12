ca902c9cffcd155eec1220b812e8c5b709dd6a35
Patilkartik1 <132817019+PatilKartik1@users.noreply.github.com>
2026-05-12 23:41:43 +0530

    addeed browser router in main and routes and rout in app.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,3 @@
-import "./index.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -8,6 +6,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
